750ea5d2b57b1bc4ac8eece36814f396a3e76ef7
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-11-24 00:59:17 +0100

    workaround for Unknown VCS

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject friboo-ext-zalando/lein-template "2.0.0-beta1-SNAPSHOT"
+(defproject friboo-ext-zalando/lein-template "2.0.0-beta1"
   :description "Leiningen template for friboo-ext-zalando framework"
   :url "https://github.com/zalando-incubator/friboo-ext-zalando/template"
   :license {:name "Apache License"
@@ -14,6 +14,7 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
+  :vcs :git
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/java.classpath "0.2.3"]
